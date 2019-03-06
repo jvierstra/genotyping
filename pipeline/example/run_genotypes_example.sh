234e102cebd0a0c4ec4c004f95ae9058bac573aa
sjneph <sjneph@gmail.com>
2019-03-06 09:12:32 -0800

    update example

--- pipeline/example/run_genotypes_example.sh
+++ pipeline/example/run_genotypes_example.sh
@@ -8,7 +8,7 @@ outdir=results.tcells
 FASTA_CHROM_FILE=/net/seq/data/genomes/human/GRCh38/noalts/GRCh38_no_alts.chrom_sizes.bed
 FASTA_FILE=/net/seq/data/genomes/human/GRCh38/noalts/GRCh38_no_alts.fa
 
-script=../../call_genotypes.sh
+script=../call_genotypes.sh
 
 chmod 755 $script
 $script $ddir $outdir $FASTA_CHROM_FILE $FASTA_FILE
